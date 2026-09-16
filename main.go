a48c569d2e7cac7ee6257001a7661f74c502871a
Harshit Mani Tripathi <harshit@hudater.dev>
2026-09-17 00:13:13 +0530

    chapter 1

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var interval_seconds int
+	var job_status string
+	name := "Worker"
+
+	fmt.Printf("Worker name: '%v' at Status: '%v' after Interval: '%v'\n", name, job_status, interval_seconds)
+}
